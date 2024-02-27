dd7eba958f0717284173963915d58c07662794d8
antoniocristovam <antoniocristovam@outlook.com.br>
2024-02-26 21:14:01 -0300

    feat: add badge

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Barbershop } from "@prisma/client";
 import Image from "next/image";
 import { Button } from "@/app/components/ui/button";
+import { Badge } from "@/app/components/ui/badge";
+import { StarIcon } from "lucide-react";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
@@ -14,6 +16,15 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="relative w-full h-[159px]">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge
+              variant={"secondary"}
+              className="opacity-90 flex gap-1 items-center absolute top=3 left-3 z-50"
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
           <Image
             alt={barbershop.name}
             src={barbershop.imageUrl}
